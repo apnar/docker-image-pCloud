22e26d2dd40ae63d4a88f310689dc8c603a14655
DjSni <31524614+DjSni@users.noreply.github.com>
2022-02-14 12:41:17 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,7 +8,8 @@ set -e
 if [ ! -f /root/.pcloud/data.db ]
 then
   echo "Starting pCloud First Run - Enter Password"
-  exec /usr/bin/pcloudcc --username ${PCLOUD_USER} --mountpoint ${PCLOUD_MOUNT} --password --savepassword
+  exec /bin/bash
+  #exec /usr/bin/pcloudcc --username ${PCLOUD_USER} --mountpoint ${PCLOUD_MOUNT} --password --savepassword
 fi
 
 
